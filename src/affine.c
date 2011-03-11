8c8435d2a636a14ffdd3174cd1b6cc6afb9ff6e3
jonwright <wright@esrf.fr>
2011-03-11 15:49:46 +0100

    Refactor to use array typedefs

--- src/affine.c
+++ src/affine.c
@@ -29,7 +29,7 @@ double degrees(double p){ return p*180.0/M_PI; }
  * @param b
  * @return a.b
  */
-double dot3( double a[], double b[]){
+scalar dot3( vector a, vector b){
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 }
 
@@ -40,10 +40,10 @@ double dot3( double a[], double b[]){
  * @param b
  * @return a x b
  */
-void cross3(double a[], double b[], double c[]){
-    c[0] = a[1]*b[2] - a[2]*b[1];
-    c[1] = a[2]*b[0] - a[0]*b[2];
-    c[2] = a[0]*b[1] - a[1]*b[0];
+void cross3(vector a, vector b, vector *c){
+    (*c)[0] = a[1]*b[2] - a[2]*b[1];
+    (*c)[1] = a[2]*b[0] - a[0]*b[2];
+    (*c)[2] = a[0]*b[1] - a[1]*b[0];
 }
 /**
  * Computes a 3x3 matrix vector product 
@@ -52,10 +52,10 @@ void cross3(double a[], double b[], double c[]){
  * @param double[3] v is a vector
  * @param double[3] r is the result
  */
-void mat3_transform_vec( double m[], double v[], double r[]){
-    r[0] = m[0] * v[0] + m[1]*v[1] + m[2]*v[2];
-    r[1] = m[3] * v[0] + m[4]*v[1] + m[5]*v[2];
-    r[2] = m[6] * v[0] + m[7]*v[1] + m[8]*v[2];
+void mat3_transform_vec( rmatrix m, vector v, vector *r){
+    (*r)[0] = m[0] * v[0] + m[1]*v[1] + m[2]*v[2];
+    (*r)[1] = m[3] * v[0] + m[4]*v[1] + m[5]*v[2];
+    (*r)[2] = m[6] * v[0] + m[7]*v[1] + m[8]*v[2];
 }
 
 
@@ -72,47 +72,43 @@ void mat3_transform_vec( double m[], double v[], double r[]){
  * @param double[9] c 
  * @return error status
  */
-void mat3_prod( double a[], double b[], double c[]){
-    c[0] = a[0]*b[0] + a[1]*b[3] + a[2]*b[6];
-    c[1] = a[0]*b[1] + a[1]*b[4] + a[2]*b[7];
-    c[2] = a[0]*b[2] + a[1]*b[5] + a[2]*b[8];
-    c[3] = a[3]*b[0] + a[4]*b[3] + a[5]*b[6];
-    c[4] = a[3]*b[1] + a[4]*b[4] + a[5]*b[7];
-    c[5] = a[3]*b[2] + a[4]*b[5] + a[5]*b[8];
-    c[6] = a[6]*b[0] + a[7]*b[3] + a[8]*b[6];
-    c[7] = a[6]*b[1] + a[7]*b[4] + a[8]*b[7];
-    c[8] = a[6]*b[2] + a[7]*b[5] + a[8]*b[8];
+void mat3_prod( rmatrix a, rmatrix b, rmatrix *c){
+    (*c)[0] = a[0]*b[0] + a[1]*b[3] + a[2]*b[6];
+    (*c)[1] = a[0]*b[1] + a[1]*b[4] + a[2]*b[7];
+    (*c)[2] = a[0]*b[2] + a[1]*b[5] + a[2]*b[8];
+    (*c)[3] = a[3]*b[0] + a[4]*b[3] + a[5]*b[6];
+    (*c)[4] = a[3]*b[1] + a[4]*b[4] + a[5]*b[7];
+    (*c)[5] = a[3]*b[2] + a[4]*b[5] + a[5]*b[8];
+    (*c)[6] = a[6]*b[0] + a[7]*b[3] + a[8]*b[6];
+    (*c)[7] = a[6]*b[1] + a[7]*b[4] + a[8]*b[7];
+    (*c)[8] = a[6]*b[2] + a[7]*b[5] + a[8]*b[8];
 }
 
 /**
  * Does an affine transform matrix vector product 
  *
- * The last row of the matrix which is (0,0,0,1) is not stored
- * The matrix storage is the same as a usual 3x3 with a vector 
- * at the end
- * [ 0  1  2 ]   v0   9    v0*0 +  v1*1 + v2*2 + v9
- * [ 3  4  5 ] . v1 + 10 = v0*3 +  v1*4 + v2*5 + v10
- * [ 6  7  8 ]   v2   11   v0*6 +  v1*7 + v2*8 + v11
+ * The affine matrix + vector is supplied in a struct
+ * as (m,v)
  *
  * @param double[12] m is a 4x3 matrix stored in row order
  * @param double[3] v is a vector
  * @param double[3] r is the result
  * @return error status 
  */
-void affine_transform_vec( double m[], double v[], double r[]){
-    r[0] = m[0]*v[0] + m[1]*v[1] + m[2]*v[2] + m[9];
-    r[1] = m[3]*v[0] + m[4]*v[1] + m[5]*v[2] + m[10];
-    r[2] = m[6]*v[0] + m[7]*v[1] + m[8]*v[2] + m[11];
+void affine_transform_vec( afmatrix m, vector v, vector *r){
+    (*r)[0] = m.m[0]*v[0] + m.m[1]*v[1] + m.m[2]*v[2] + m.v[0];
+    (*r)[1] = m.m[3]*v[0] + m.m[4]*v[1] + m.m[5]*v[2] + m.v[1];
+    (*r)[2] = m.m[6]*v[0] + m.m[7]*v[1] + m.m[8]*v[2] + m.v[2];
 }
 
 
 /**
  * Finds the determinant of a 3x3 matrix
  *
- * @param double[9] m is a 3x3 matrix with row major storage
- * @return double determinant
+ * @param rmatrix is a 3x3 matrix with row major storage
+ * @return determinant
  */
-double determinant3( double m[] ){
+scalar determinant3( rmatrix m ){
     return m[0]*m[4]*m[8] - m[0]*m[5]*m[7] + m[1]*m[5]*m[6] - 
            m[1]*m[3]*m[8] + m[2]*m[3]*m[7] - m[2]*m[4]*m[6];
 }
@@ -129,22 +125,22 @@ double determinant3( double m[] ){
  * @param r will be the inverse 
  * @return error if the determinant is zero
  */
-int inverse_mat3(double m[], double r[]){
-    double d;
+int inverse_mat3(rmatrix m, rmatrix *r){
+    scalar d;
     d = determinant3( m );
     if (fabs(d) == 0.0F){  
         return 1;
     } 
     d = 1.0f/d;
-    r[0] =  m[4]*m[8]*d - m[7]*m[5]*d;
-    r[1] =  m[7]*m[2]*d - m[8]*m[1]*d;
-    r[2] =  m[1]*m[5]*d - m[2]*m[4]*d;
-    r[3] =  m[5]*m[6]*d - m[3]*m[8]*d;
-    r[4] =  m[8]*m[0]*d - m[2]*m[6]*d;
-    r[5] =  m[2]*m[3]*d - m[0]*m[5]*d;
-    r[6] =  m[3]*m[7]*d - m[6]*m[4]*d;
-    r[7] =  m[1]*m[6]*d - m[0]*m[7]*d;
-    r[8] =  m[0]*m[4]*d - m[3]*m[1]*d;
+    (*r)[0] =  m[4]*m[8]*d - m[7]*m[5]*d;
+    (*r)[1] =  m[7]*m[2]*d - m[8]*m[1]*d;
+    (*r)[2] =  m[1]*m[5]*d - m[2]*m[4]*d;
+    (*r)[3] =  m[5]*m[6]*d - m[3]*m[8]*d;
+    (*r)[4] =  m[8]*m[0]*d - m[2]*m[6]*d;
+    (*r)[5] =  m[2]*m[3]*d - m[0]*m[5]*d;
+    (*r)[6] =  m[3]*m[7]*d - m[6]*m[4]*d;
+    (*r)[7] =  m[1]*m[6]*d - m[0]*m[7]*d;
+    (*r)[8] =  m[0]*m[4]*d - m[3]*m[1]*d;
     return 0;
 }
 
@@ -160,17 +156,16 @@ int inverse_mat3(double m[], double r[]){
  * @param r is the resulting inverse
  * @return error if determinant is zero
  */
-int inverse_affine_mat(double m[], double r[]){
+int inverse_affine_mat(afmatrix m, afmatrix *r){
     int err;
-    double t[3];
-    err = inverse_mat3( m, r );
+    err = inverse_mat3( m.m, &(*r).m );
     if(err != 0){
         return 1;
     }
-    mat3_transform_vec(r, &m[9], t);
-    r[9] = -t[0];
-    r[10] = -t[1];
-    r[11] = -t[2];
+    mat3_transform_vec((*r).m, m.v, &(*r).v);
+    (*r).v[0] = -(*r).v[0];
+    (*r).v[1] = -(*r).v[1];
+    (*r).v[2] = -(*r).v[2];
     return 0;
 }
 
@@ -180,7 +175,7 @@ int inverse_affine_mat(double m[], double r[]){
  * @param a length 3 vector
  * @return sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2])
  */
-double norm3(double v[]){
+scalar norm3(rmatrix v){
     return sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
 }
 
@@ -190,13 +185,13 @@ double norm3(double v[]){
  * @param a length 3 double vector
  * @return error if the vector is zero
  */
-int normalise_vector(double v[]){
-    double norm;
-    norm = norm3(v);
+int normalise_vector(vector *v){
+    scalar norm;
+    norm = norm3(*v);
     if( norm > 0 ){
-       v[0] = v[0]/norm;
-       v[1] = v[1]/norm;
-       v[2] = v[2]/norm;
+       (*v)[0] = (*v)[0]/norm;
+       (*v)[1] = (*v)[1]/norm;
+       (*v)[2] = (*v)[2]/norm;
        return 0;
     }
     return 1;
@@ -216,7 +211,7 @@ int normalise_vector(double v[]){
  * @param m = the matrix to be computed
  * @return error if the vector cannot be normalised
  */
-int mat3_from_axis_angle(double a[], double p, double m[]){
+int mat3_from_axis_angle(vector *a, scalar p, rmatrix *m){
     int err;
     double c,s,t,T1,T2;
     err = normalise_vector(a);
@@ -224,21 +219,21 @@ int mat3_from_axis_angle(double a[], double p, double m[]){
     s = sin(p);
     c = cos(p);
     t = 1-c;
-    m[0] = t*a[0]*a[0] + c;
-    m[4] = t*a[1]*a[1] + c;
-    m[8] = t*a[2]*a[2] + c;
-    T1 = t*a[0]*a[1];
-    T2 = a[2]*s;
-    m[1] =  T1 - T2;
-    m[3] =  T1 + T2;
-    T1 = t*a[1]*a[2];
-    T2 =  a[0]*s;  
-    m[5] = T1 - T2;
-    m[7] = T1 + T2;
-    T1 = t*a[0]*a[2]; 
-    T2 = a[1]*s;
-    m[2] = T1 + T2;
-    m[6] = T1 - T2;
+    (*m)[0] = t*(*a)[0]*(*a)[0] + c;
+    (*m)[4] = t*(*a)[1]*(*a)[1] + c;
+    (*m)[8] = t*(*a)[2]*(*a)[2] + c;
+    T1 = t*(*a)[0]*(*a)[1];
+    T2 = (*a)[2]*s;
+    (*m)[1] =  T1 - T2;
+    (*m)[3] =  T1 + T2;
+    T1 = t*(*a)[1]*(*a)[2];
+    T2 =  (*a)[0]*s;  
+    (*m)[5] = T1 - T2;
+    (*m)[7] = T1 + T2;
+    T1 = t*(*a)[0]*(*a)[2]; 
+    T2 = (*a)[1]*s;
+    (*m)[2] = T1 + T2;
+    (*m)[6] = T1 - T2;
     return 0;
 }
 
@@ -255,19 +250,19 @@ int mat3_from_axis_angle(double a[], double p, double m[]){
  * @param v[3] is the input point
  * @param r[3] is the ouput point
  */
-int rotate_vector_axis_angle(double a[], double p, double v[], double r[]){
-    double sinp, cosp, tmp;
-    double t[3];
+int rotate_vector_axis_angle(vector *a, scalar p, vector v, vector *r){
+    scalar sinp, cosp, tmp;
+    vector t;
     int err;
     err = normalise_vector(a);
     if (err != 0){ return err;}
     cosp = cos(p);
     sinp = sin(p);
-    tmp = dot3(a,v)*(1-cosp);
-    cross3(v,a,t);
-    r[0] = v[0] * cosp + tmp*a[0] + t[0]*sinp ;
-    r[1] = v[1] * cosp + tmp*a[1] + t[1]*sinp ;
-    r[2] = v[2] * cosp + tmp*a[2] + t[2]*sinp ;
+    tmp = dot3((*a),v)*(1-cosp);
+    cross3(v,(*a),&t);
+    (*r)[0] = v[0] * cosp + tmp*(*a)[0] + t[0]*sinp ;
+    (*r)[1] = v[1] * cosp + tmp*(*a)[1] + t[1]*sinp ;
+    (*r)[2] = v[2] * cosp + tmp*(*a)[2] + t[2]*sinp ;
     return 0;
 }
 
--- test/testaffine.c
+++ test/testaffine.c
@@ -21,25 +21,25 @@ int tests_run=0;
 
 
 char* test_mat3_transform_vec(void){
-    double vec[3] = { 1, 2, 3 };
-    double mat[9] = { 0, 0, 1, 
-                     0, 1, 0, 
-                     1, 0, 0 };
-    double t[3];
-    mat3_transform_vec( mat, vec, t );
-    MU_ASSERT( "transform_vec0", t[0] == vec[2] );
-    MU_ASSERT( "transform_vec1", t[1] == vec[1] );
-    MU_ASSERT( "transform_vec2", t[2] == vec[0] );
+    vector vec = { 1, 2, 3 };
+    rmatrix mat = { 0, 0, 1, 
+                    0, 1, 0, 
+                    1, 0, 0 };
+    vector t;
+    mat3_transform_vec( mat, vec, &t );
+    MU_ASSERT( "mat3_transform_vec0", fabs(t[0] - vec[2])<1e-6 );
+    MU_ASSERT( "mat3_transform_vec1", fabs(t[1] - vec[1])<1e-6 );
+    MU_ASSERT( "mat3_transform_vec2", fabs(t[2] - vec[0])<1e-6 );
     return 0;
  
 }
 
 char* test_determinant3(void){
-    double a[9] = { 0.30112582,  0.67285109,  0.37710869,  
+    rmatrix a = { 0.30112582,  0.67285109,  0.37710869,  
                     0.43101797,  0.73377317,  0.60827196,  
                     0.42625684,  0.3349345 ,  0.9300366 };
-    double d = -0.014623566951869022;
-    double t;
+    scalar d = -0.014623566951869022;
+    scalar t;
 
     t = determinant3( a );
     MU_ASSERT( "determinant OK", fabs( t - d) < 1e-6 );
@@ -47,15 +47,15 @@ char* test_determinant3(void){
 }
 
 char* test_inverse_mat3(void){
-    double a[9] = { 0.30112582,  0.67285109,  0.37710869,  
+    rmatrix a = { 0.30112582,  0.67285109,  0.37710869,  
                     0.43101797,  0.73377317,  0.60827196,  
                     0.42625684,  0.3349345 ,  0.9300366 };
-    double e[9] = {-32.73514938,  34.15510178,  -9.06510735,
+    rmatrix e = {-32.73514938,  34.15510178,  -9.06510735,
                   9.68179703,  -8.15894508,   1.41044748,
                  11.51655006, -12.71577263,   4.72202539 };
-    double t[9];
+    rmatrix t;
     int i, ret;
-    MU_ASSERT( "inverse runs", inverse_mat3( a, t) == 0 );
+    MU_ASSERT( "inverse runs", inverse_mat3( a, &t) == 0 );
     ret = 0;
     for( i=0; i<9; i++){
         if (fabs(e[i]-t[i])/fabs(t[i])>1e-6){
@@ -68,32 +68,32 @@ char* test_inverse_mat3(void){
 
 
 char* test_affine_transform_vec(void){
-    double m[12] = { 0, 0, 1, 
+    afmatrix m = { 0, 0, 1, 
                     0, 1, 0,
                     1, 0, 0,
                     0.1,0.2,0.3};
-    double v[3] = { 3,4,5 };
-    double r[3];
-    affine_transform_vec( m, v, r );
-    MU_ASSERT("transform_vec0",fabs(r[0] - 5.1)<1e-6);
-    MU_ASSERT("transform_vec1",fabs(r[1] - 4.2)<1e-6);
-    MU_ASSERT("transform_vec2",fabs(r[2] - 3.3)<1e-6);
+    vector v = { 3,4,5 };
+    vector r;
+    affine_transform_vec( m, v, &r );
+    MU_ASSERT("affinetransform_vec0",fabs(r[0] - 5.1)<1e-6);
+    MU_ASSERT("affinetransform_vec1",fabs(r[1] - 4.2)<1e-6);
+    MU_ASSERT("affinetransform_vec2",fabs(r[2] - 3.3)<1e-6);
     return 0;
 }
 
 char* test_mat3_prod(void){
-    double a[9] = {  0.97300866,  0.53048923,  0.32333488,  
+    rmatrix a   = { 0.97300866,  0.53048923,  0.32333488,  
                     0.6900934 ,  0.44364110,  0.68283224,  
                     0.51383945,  0.87587622,  0.80127724 };
-    double b[9] = {  0.06703767,  0.85926415,  0.38202935,  
+    rmatrix b   = { 0.06703767,  0.85926415,  0.38202935,  
                     0.68021704,  0.29180251,  0.38910406, 
                     0.59501276,  0.50158507,  0.07206758 };
-    double c[9] = {  0.61846443,  1.15304949,  0.60143535, 
+    rmatrix c   = { 0.61846443,  1.15304949,  0.60143535, 
                     0.75432839,  1.06492656,  0.48546856,  
                     1.10700271,  1.09901539,  0.59485486 };
-    double t[9];
+    rmatrix t;
     int i;
-    mat3_prod( a, b, t);
+    mat3_prod( a, b, &t);
     for( i=0 ; i<9 ; i++)
         MU_ASSERT( "mat3_prod", fabs((c[i]-t[i])/c[i])<1e-6 );
     return 0;
@@ -101,44 +101,45 @@ char* test_mat3_prod(void){
 
 
 char* test_inverse_affine_mat(void){
-    double a[3] = {4,5,6};
-    double m[12] = { 0,0,1,  1,0,0, 0,1,0, 0.1, 0.2, 0.3 };
-    double r[3];
-    double im[12];
-    double ir[3];
+    vector a = {4,5,6};
+    afmatrix m = { 0,0,1,  1,0,0, 0,1,0, 0.1, 0.2, 0.3 };
+    vector r;
+    afmatrix im;
+    vector ir;
     int i;
-    affine_transform_vec(m, a, r);
-    inverse_affine_mat(m, im);
-    affine_transform_vec(im, r, ir);
+    affine_transform_vec(m, a, &r);
+    inverse_affine_mat(m, &im);
+    affine_transform_vec(im, r, &ir);
     for(i=0;i<3;i++)
         MU_ASSERT("inverse affine OK", fabs( ( ir[i] - a[i] )/a[i] ) < 1e-6);
     return 0;
 }
 
 char* test_norm3(void){
-    double t[3] = {4,5,6};
+    vector t = {4,5,6};
     MU_ASSERT( "norm3 OK", fabs(norm3(t)- sqrt(77)) < 1e-6);
     return 0;
 }
 
 char* test_normalise_vector(void){
-    double v[3] = {2,2,2};
+    vector v = {2,2,2};
     int i;
-    normalise_vector(v);
+    normalise_vector(&v);
     for(i=0;i<3;i++)
         MU_ASSERT("Normalise vector",fabs(3*v[i]*v[i] - 1)<1e-6);
     return 0;
 }
 
 char* test_mat3_axis_angle(void){
-    double m[9];
-    double a[3] = {0, 0, 1};
-    double t[9],s30,c30;
-    double p=30.0;
+    rmatrix m;
+    vector a = {0, 0, 1};
+    rmatrix t;
+    scalar s30,c30;
+    scalar p=30.0;
     int err,i;
     p = radians( 30.0 ) ;
-    err = mat3_from_axis_angle( a, p, m );
-    c30 = sqrt(3)/2.; /*  cos(30) */
+    err = mat3_from_axis_angle( &a, p, &m );
+    c30 = sqrt(3)/2.; /*   cos(30) */
     s30 = 0.5  ;      /*   sin(30) */
     t[0] = c30; t[1] = -s30; t[2]=0; 
     t[3] = s30; t[4] =  c30; t[5]=0; 
